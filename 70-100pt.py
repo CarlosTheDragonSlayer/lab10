215b9e895b81df7f9250b19667eea5cbc595aca3
CarlosTheDragonSlayer <carlos.ojeda.gymnastics@gmail.com>
2014-10-21 14:21:06 -0500

    Commit 100 pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -19,10 +19,27 @@ from Tkinter import *
 root = Tk()
 
 # Create the canvas widget
-drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad = Canvas(root, width=800,height=600, background = 'white')
 drawpad.grid(row=0, column=1)
 
-# Insert your code here to draw the house!
+# Outline of House
+rectangle1 = drawpad.create_rectangle(300,200,500,400, fill = 'red')
+line1 = drawpad.create_line(300,200,400,100)
+line2 = drawpad.create_line(500,200,400,100)
 
+# Door and Windows
+door = drawpad.create_rectangle(380,330,420,400, fill = 'white')
+doorhandle = drawpad.create_oval(385,360,392,367)
+window1 = drawpad.create_rectangle(325,230,375,280, fill = 'white')
+window2 = drawpad.create_rectangle(425,230,475,280, fill = 'white')
+
+
+# Chimney
+chimney1 = drawpad.create_line(330,50,330,170)
+chimney2 = drawpad.create_line(360,50,360,140)
+chimneytop = drawpad.create_line(330,50,360,50)
+
+# grass
+ground = drawpad.create_rectangle(200,400,600,500, fill = 'green')
 
 root.mainloop()
